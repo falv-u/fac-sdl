87cdbdd0351d016d4a40613dd2f15b198066d64b
falvcont <falvcont@umagallanes.cl>
2026-06-04 14:52:13 +0000

    esqueleto y leve modificacion, usando sdl

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+int main(void)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Quit();
+	return 0;
+}
---
+++ xmake.lua
@@ -0,0 +1,20 @@
+set_project("fac") set_languages("c11")
+
+add_rules("mode.debug", "mode.release")
+
+target("game")
+   set_kind("binary")
+   add_files("src/*.c")
+	add_includedirs("include")
+   add_packages("libsdl")
+	if is_plat("linux") then
+      add_cflags("-Wall", "-Wextra", {tools = "clang"})
+
+      if is_mode("debug") then
+         add_cflags("-g", "-fsanitize=address,undefined", {tools = "clang"})
+         add_ldflags("-fsanitize=address,undefined", {tools = "clang"})
+      	add_cflags("-O3", {tools = "clang"})
+			set_policy("build.sanitizer.address", true)
+ 			set_policy("build.sanitizer.undefined", true)
+      end
+   end

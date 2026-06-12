dd8eacb287fb34791807b5893d46b806028cb599
falvcont <falvcont@umagallanes.cl>
2026-06-12 18:23:35 +0000

    subiendo archivos que por error no se subieron al repo

---
+++ src/commons.c
@@ -0,0 +1,21 @@
+#include "commons.h"
+
+/* funciones utiles */
+
+void iniciar_componente(bool test, const char *componente) {
+	if (test) {
+		return;
+	}
+	fprintf(stderr, "no se pudo iniciar %s\n",componente); /* RECORDAR: stderr es exclusivo de errores */
+}
+
+void Pantalla_Completa(SDL_Window *ventana)
+{
+    Uint32 flags_actuales = SDL_GetWindowFlags(ventana);
+
+    if (flags_actuales & SDL_WINDOW_FULLSCREEN_DESKTOP) {
+        SDL_SetWindowFullscreen(ventana, 0);
+    } else {
+        SDL_SetWindowFullscreen(ventana, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    }}
+
---
+++ src/menu.c
@@ -0,0 +1,44 @@
+#include "SDL_events.h"
+#include "SDL_image.h"
+#include "SDL_keycode.h"
+#include "commons.h"
+
+void renderizar_fractales(eventos_globales ev_gl);
+ESTADO_ACTUAL menu_principal(eventos_globales ev_gl, SDL_Event *evento, ESTADO_ACTUAL estado_actual, menu_principal_recursos *rec_menu)
+{
+
+	SDL_RenderPresent(ev_gl.renderizado);
+	SDL_Surface *surfaceTexto = TTF_RenderText_Solid(rec_menu->fuente, "Feel, Amplify and Conquer!", rec_menu->color1);
+	SDL_Texture *texturaTexto = SDL_CreateTextureFromSurface(ev_gl.renderizado, surfaceTexto);
+	
+	SDL_Rect rectDestino = {100, 100, surfaceTexto->w, surfaceTexto->h };
+	SDL_FreeSurface(surfaceTexto);
+   SDL_RenderCopy(ev_gl.renderizado, texturaTexto, NULL, &rectDestino);
+	
+	SDL_DestroyTexture(texturaTexto);
+	SDL_Surface *imagen = IMG_Load("/home/frani/code/fac-sdl/assets/puf.png");
+	SDL_Texture* textura = SDL_CreateTextureFromSurface(ev_gl.renderizado, imagen);
+	SDL_FreeSurface(imagen); 
+   SDL_RenderClear(ev_gl.renderizado);
+   SDL_RenderCopy(ev_gl.renderizado, textura, NULL, NULL);;
+	if (evento->type == SDL_KEYDOWN)
+	{
+		
+		if (evento->key.keysym.sym == SDLK_ESCAPE) 
+			return ESTADO_SALIR;
+		
+		if (evento->key.keysym.sym == SDLK_RETURN) 
+			return ESTADO_JUEGO;
+	}
+
+
+	SDL_RenderPresent(ev_gl.renderizado);
+	return estado_actual;
+}
+
+
+void renderizar_fractales(eventos_globales ev_gl)
+{
+
+}
+
